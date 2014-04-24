00843615edc519ee9fd90eaff869eefeffa15a04
metulburr <micahpage911@yahoo.com>
2014-04-24 05:20:29 -0400

    init commit

---
+++ WSOPLauncher.py
@@ -0,0 +1,22 @@
+
+#appname
+#HKEY_CURRENT_USER > Software > Classes > Extensions > Contractld > Windows.Protocol >Packageld > ActivableClassId > {APPNAME} > CustomProperties > {name value}
+
+
+
+import subprocess
+name = 'xboxliveapp-1297292137'
+start = r'C:\Windows\System32\cmd.exe /c start {}:'.format(name)
+kill = r'taskkill /IM WSOP.exe /F'
+
+subprocess.Popen(start.split())
+
+
+while True:
+    ans = raw_input('Press y to kill WSOP. [y/n]')
+    if ans.lower() == 'y':
+        print()
+        subprocess.Popen(kill.split())
+        break
+
+
